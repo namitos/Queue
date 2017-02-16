2a9fb8199ce65153a3288f78b595591e88417332
Artem <namitos@users.noreply.github.com>
2017-02-16 21:04:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 'use strict';
 
 module.exports = class Queue extends Array {
-	run(data, i) {
-		var promises = this;
-		i = i || 0;
-		if (promises.length > 0) {
-			return promises[i](data).then(function (data) {
-				if (!promises[i + 1]) {
-					return new Promise(function (resolve) {
-						resolve(data);
-					});
-				} else {
-					return promises.run(data, i + 1);
-				}
-			});
-		} else {
-			return new Promise(function (resolve) {
-				resolve(data);
-			});
-		}
-	}
+  run(data = {}, i = 0) {
+    return this.length ? this[i](data).then((data) => this[i + 1] ? this.run(data, i + 1) : Promise.resolve(data)) : Promise.resolve(data);
+  }
 }
